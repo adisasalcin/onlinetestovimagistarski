4516ad94778b6d01929d0af2d71615077ec3cb15
Adisa Salčin <salcin.adisa@yahoo.com>
2018-08-31 13:54:54 +0200

    link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,12 +15,12 @@ class HomePage extends React.Component {
           <div className="baner-text">
           <h1>Dobro došli </h1>
           <hr />
-<div id="linkoviPocetna">
-              <a href="/BTest1"> B kategorija |</a>
-              <a href="/CTest"> C kategorija |</a>
-              <a href="/DTest"> D kategorija |</a>
-                <a href="/PrvaPomoc1"> Prva Pomoć</a>
-</div>
+<p>
+              <a href="/BTest1" className="linkoviPocetna"> B kategorija |</a>
+              <a href="/CTest" className="linkoviPocetna"> C kategorija |</a>
+              <a href="/DTest" className="linkoviPocetna"> D kategorija |</a>
+                <a href="/PrvaPomoc1" className="linkoviPocetna"> Prva Pomoć</a>
+</p>
             <div className="mreze-linkovi">
                 <a href="https://www.linkedin.com/" rel="noopener nofererrer">
                             <i className="fa fa-linkedin-square" /></a>
